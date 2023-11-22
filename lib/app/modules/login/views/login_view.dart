396681c8dd2362b62ff9b94991e2664efc8f8028
mrefaat <mrefaat@getgroup.com>
2023-11-22 11:02:08 +0200

    authenciation

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -13,6 +13,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
+
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
