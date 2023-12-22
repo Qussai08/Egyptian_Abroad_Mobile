cd6979fb1086eefdf77c03413c74fe8e699915f4
Peter Wahba <peter.mounir4@gmail.com>
2023-12-22 21:42:18 +0200

    update bottom sheet

--- lib/app/modules/bottom_navigation.dart/bindings/bottom_navigation_binding.dart
+++ lib/app/modules/bottom_navigation.dart/bindings/bottom_navigation_binding.dart
@@ -1,5 +1,4 @@
 import 'package:egyptians_abroad/app/modules/bottom_navigation.dart/controllers/bottom_navigation_controller.dart';
-import 'package:egyptians_abroad/app/modules/home/controllers/favorites_controller.dart';
 import 'package:egyptians_abroad/app/modules/home/data/providers/favorites_list_provider.dart';
 import 'package:egyptians_abroad/app/modules/more/controllers/more_controller.dart';
 import 'package:egyptians_abroad/app/modules/start_service/controllers/start_service_controller.dart';
@@ -13,9 +12,11 @@ import '../../notifications/data/providers/notifications_provider.dart';
 class BottomNavigationBinding extends Bindings {
   @override
   void dependencies() {
-    // Home
+    // Favorites
     Get.put<FavoritesListProvider>(FavoritesListProvider());
     Get.put<StartServiceController>(StartServiceController());
+
+    // Home
     Get.put<HomeController>(HomeController());
     // Get.put<HomeController>(HomeController());
     Get.put<HomeShowcaseController>(HomeShowcaseController());
