bb1a12f4c0f88dccac17b9380bbc6b5a3381658c
mrefaat <mrefaat@getgroup.com>
2024-01-09 22:50:11 +0200

    version 1.2.9

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -76,7 +76,7 @@ class SplashView extends StatelessWidget {
                       ],
                     ),
                     child: Text(
-                      "v1.2.8",
+                      "v1.2.9",
                       style: Styles.getMediumStyle(
                           color: Styles.black3.withOpacity(0.9), fontSize: 17),
                     ))
