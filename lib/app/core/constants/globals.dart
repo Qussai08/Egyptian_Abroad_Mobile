1d61aff20eee5716e8d35317d4c4712b6dfef259
Qussai08 <mahmoudqussai96@gmail.com>
2024-02-21 10:56:42 +0200

    fix ssl isssue

--- lib/app/core/constants/globals.dart
+++ lib/app/core/constants/globals.dart
@@ -3,9 +3,10 @@ class Constants {
 
   static const String devUrl = "http://10.3.21.22:7200/App/";
   static const String testingUrl = "http://10.3.20.230:7200/App/";
-  static const String uatUrl =
-      "https://demo1egabroad.eastus.cloudapp.azure.com/mobgw/App/";
-  static const String baseUrl = testingUrl;
+  static const String uatUrl = "https://10.107.201.107/ebrdmgw/App/";
+  static const String uatUrl2 = "http://10.107.212.130/App/";
+  // 10.107.212.130
+  static const String baseUrl = uatUrl;
   static const String getNotificationHistory = "GetNotificationHistory";
   static const String registerFCMTokenPath = 'AssignFCMToken';
   static const String viewAccount = 'ViewAccount';
